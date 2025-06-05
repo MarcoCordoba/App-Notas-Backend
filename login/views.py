bd39f958b7d640c1aee28b2acc9068e03d733203
Marco Córdoba <marco.cba2@gmail.com>
2025-06-05 11:08:48 -0300

    implementacion de login y registro con token

--- login/views.py
+++ login/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
-# Create your views here.
+@api_view(['POST'])
+def login(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    user = get_object_or_404(User, username=username)
+
+    if not user.check_password(password):
+        return Response({"error": "Invalid password"}, status=400)
+
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(instance=user)
+
+    return Response({"token": token.key, "user": serializer.data})
+
+
+@api_view(['POST'])
+def register(request):
+    serializer = UserSerializer(data=request.data)
+
+    if serializer.is_valid():
+        user = serializer.save()
+        user.set_password(serializer.validated_data['password'])
+        user.save()
+
+        token = Token.objects.create(user=user)
+        return Response({'token': token.key, 'user': serializer.data})
+
+    return Response(serializer.errors, status=400)
+
+
+@api_view(['POST'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def profile(request):
+
+    
+
+
+    return Response({})
--- notas/urls.py
+++ notas/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tareas/', include('tareas.urls')),
+    path('api/', include('login.urls')),
 ]
